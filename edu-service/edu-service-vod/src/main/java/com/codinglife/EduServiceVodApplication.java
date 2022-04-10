6b15626f89af832ae5fac7497e75b85c0c72981a
CodingLifeVV <wyj_program@163.com>
2022-04-10 10:41:20 +0800

    忽略一些不必要上传的文件

---
+++ edu-service/edu-service-vod/src/main/java/com/codinglife/EduServiceVodApplication.java
@@ -0,0 +1,13 @@
+package com.codinglife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduServiceVodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduServiceVodApplication.class, args);
+    }
+
+}
